ca4f0cbb51c9f249aa5a0294171c1bc8af6ff1d2
Billy Dotson <billybad5@gmail.com>
2022-09-22 16:12:35 -0400

    Add project files.

---
+++ BetterCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Calculator!");
+
+Console.WriteLine("Enter a number: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter Operator: ");
+string op = Console.ReadLine();
+
+Console.WriteLine("Enter a number: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+if (op == "+") {
+    Console.Write(num1 + num2);
+} else if (op == "-") {
+    Console.Write(num1 - num2);
+}else if (op == "*") {
+    Console.Write(num1 * num2);
+} else if (op == "/") {
+    Console.Write(num1 / num2);
+} else
+{
+    Console.WriteLine("Invalid Operator");
+}
